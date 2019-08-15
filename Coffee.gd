2d14338ecc546e2fa78d9e144ec464791496269d
Raf Ceuls <rceuls@gmail.com>
2019-08-15 16:12:08 +0200

    fixed denial function

--- Coffee.gd
+++ Coffee.gd
@@ -2,11 +2,10 @@ extends RigidBody2D
 
 export var min_speed = 150  # Minimum speed range.
 export var max_speed = 250  # Maximum speed range.
-export var is_hostile = true
-var mob_types = ["fly"]
+export var is_hostile = false
 
 func _ready():
-	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	pass 
 
 func _on_Visibility_screen_exited():
 	queue_free()
